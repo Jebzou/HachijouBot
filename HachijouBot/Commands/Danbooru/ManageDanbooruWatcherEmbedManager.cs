28fa44d54e1221472c17e5d5bba9b9bb394f623c
Jebzou <22751386+Jebzou@users.noreply.github.com>
2022-12-09 12:11:04 +0100

    Fix

--- HachijouBot/Commands/Danbooru/ManageDanbooruWatcherEmbedManager.cs
+++ HachijouBot/Commands/Danbooru/ManageDanbooruWatcherEmbedManager.cs
@@ -57,7 +57,8 @@ namespace HachijouBot.Commands.ManageDatabase
             if (arg.User.Id != UserId) return;
             if (!arg.User.IsAdmin((ulong)arg.GuildId)) return;
 
-            DanbooruWatcherChannelModel? channel = DanbooruImageWatcherDataBase.Watchers.FirstOrDefault(data => arg.ChannelId == data.ChannelId);
+            // reload channel data
+            DanbooruWatcherChannelModel? channel = DanbooruImageWatcherDataBase.Watchers.FirstOrDefault(data => ChannelModel.ChannelId == data.ChannelId);
             if (channel is null) throw new Exception("No data for this channel");
             ChannelModel = channel;
 
